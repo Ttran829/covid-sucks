d66c95a17f896d70c90dd563339a2c230e883045
shannonchu519 <42817505+shannonchu519@users.noreply.github.com>
2020-11-12 19:15:36 -0500

    changed files to php

--- signup.php
+++ signup.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Freelancer - Start Bootstrap Theme</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
         <!-- Font Awesome icons (free version)-->
@@ -15,19 +14,25 @@
         <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="css/styles.css" rel="stylesheet" />
+    <style>
+        input[type="text"] {
+             display: block;
+             margin : 0 auto;
+        }
+    </style>
     </head>
     <body id="page-top">
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
             <div class="container">
-                <a class="navbar-brand js-scroll-trigger" href="#page-top">Covid Sucks</a>
+                <a class="navbar-brand js-scroll-trigger" href="login.php">Covid Sucks</a>
                 <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars"></i>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ml-auto">
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Signup</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="signup.php">Signup</a></li>
                     </ul>
                 </div>
             </div>
@@ -36,60 +41,42 @@
         <header class="masthead bg-primary text-white text-center">
             <div class="container d-flex align-items-center flex-column">
                 <!-- Masthead Avatar Image-->
-                <img class="masthead-avatar mb-5" src="assets/img/avataaars.svg" alt="" />
+                <img class="masthead-avatar mb-5" src="assets/img/facemask.png" style="width:200px" alt="" />
                 <!-- Masthead Heading-->
-                <h1 class="masthead-heading text-uppercase mb-0">Covid Sucks</h1>
+                <h1 class="masthead-heading text-uppercase mb-0">Sign up!</h1>
                 <!-- Icon Divider-->
                 <div class="divider-custom divider-light">
                     <div class="divider-custom-line"></div>
                     <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
                     <div class="divider-custom-line"></div>
                 </div>
-                <!-- Masthead Subheading-->
-                <p class="masthead-subheading font-weight-light mb-0">The easiest way to live your life safely during this suck-y pandemic</p>
+                <div style="margin:0 auto; width:600px; padding:20px; background:#f9f9f9; border-radius: 25px;">
+                    <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST" onsubmit="return signUpValidate()" style="color:black; font-size: 18px; display:block">
+                        <div class="form-group">
+                        <label for="name">Full name</label>
+                        <input name="name" id="name" type="text">
+                        </div>
+                        <div class="form-group">
+                        <label for="email">Email</label>
+                        <input name="email" id="email" type="text">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input name="password" id="password" type="text">
+                        </div>
+                        <div class="form-group">
+                            <label for="re-password">Re-enter Password</label>
+                            <input name="re-password" id="re-password" type="text">
+                        </div>
+                        <button type="signup" class="btn btn-primary" style="padding: 15px 20px; font-size:18px">Sign Up</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </header>
-        <!-- Contact Section-->
-        <section class="page-section" id="contact">
-            <div class="container">
-                <!-- Contact Section Heading-->
-                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Log In</h2>
-                <!-- Icon Divider-->
-                <div class="divider-custom">
-                    <div class="divider-custom-line"></div>
-                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                    <div class="divider-custom-line"></div>
-                </div>
-                <!-- Contact Section Form-->
-                <div class="row">
-                    <div class="col-lg-8 mx-auto">
-                        <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19.-->
-                        <form id="contactForm" name="sentMessage" novalidate="novalidate">
-                            <div class="control-group">
-                                <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                                    <label>Email Address</label>
-                                    <input class="form-control" id="email" type="email" placeholder="Email Address" required="required" data-validation-required-message="Please enter your email address." />
-                                    <p class="help-block text-danger"></p>
-                                </div>
-                            </div>
-                            <div class="control-group">
-                                <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                                    <label>Password</label>
-                                    <input class="form-control" id="phone" type="tel" placeholder="Password" required="required" data-validation-required-message="Please enter your phone number." />
-                                    <p class="help-block text-danger"></p>
-                                </div>
-                            </div>
-                            <br />
-                            <div id="success"></div>
-                            <div class="form-group"><button class="btn btn-primary btn-xl" id="sendMessageButton" type="submit">Submit</button></div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </section>
         <!-- Copyright Section-->
         <div class="copyright py-4 text-center text-white">
-            <div class="container"><small>Copyright © Your Website 2020</small></div>
+            <div class="container"><small>Copyright © Covid Sucks 2020</small></div>
         </div>
         <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes)-->
         <div class="scroll-to-top d-lg-none position-fixed">
@@ -105,5 +92,38 @@
         <script src="assets/mail/contact_me.js"></script>
         <!-- Core theme JS-->
         <script src="js/scripts.js"></script>
+    <script>
+
+        function signUpValidate(){
+            var name = document.getElementById("name");
+            var email = document.getElementById("email");
+            var password = document.getElementById("password");
+            var re_password = document.getElementById("re-password");
+
+            var passwordVal = password.value;
+            var passwordString = passwordVal.toString();
+
+            var re_password_val = re_password.value;
+            var re_password_string = re_password_val.toString();
+
+            if(name.value.length == 0){
+            alert("Name cannot be blank");
+            return false;
+            }
+
+            if(email.value.length == 0){
+            alert("Email cannot be blank");
+            return false;
+            }
+
+            if((passwordString.localeCompare(passwordConfString)) != 0){
+            alert("Passwords do not match");
+            return false;
+            }
+
+
+            return True;
+        }
+    </script>
     </body>
 </html>
