bc4924236470fb561bf3c0438d65b7720c1ab031
Ttran829 <41520668+Ttran829@users.noreply.github.com>
2020-11-14 11:39:41 -0500

    added edit and profile pages

--- edit.php
+++ edit.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Freelancer - Start Bootstrap Theme</title>
+        <title>Edit Profile</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
         <!-- Font Awesome icons (free version)-->
@@ -27,66 +27,59 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ml-auto">
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Signup</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><input type="submit" value="Save" name="action" class="btn btn-primary" title="Save Changes"></input></li>
                     </ul>
                 </div>
             </div>
         </nav>
         <!-- Masthead-->
-        <header class="masthead bg-primary text-white text-center">
-            <div class="container d-flex align-items-center flex-column">
-                <!-- Masthead Avatar Image-->
-                <img class="masthead-avatar mb-5" src="assets/img/avataaars.svg" alt="" />
+        <header class="masthead bg-primary text-white">
+            <div class="container">
                 <!-- Masthead Heading-->
-                <h1 class="masthead-heading text-uppercase mb-0">Covid Sucks</h1>
+                <h1 class="masthead-heading text-uppercase text-center mb-0">Edit Information</h1>
                 <!-- Icon Divider-->
                 <div class="divider-custom divider-light">
                     <div class="divider-custom-line"></div>
                     <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
                     <div class="divider-custom-line"></div>
                 </div>
-                <!-- Masthead Subheading-->
-                <p class="masthead-subheading font-weight-light mb-0">The easiest way to live your life safely during this suck-y pandemic</p>
-            </div>
-        </header>
-        <!-- Contact Section-->
-        <section class="page-section" id="contact">
-            <div class="container">
-                <!-- Contact Section Heading-->
-                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Log In</h2>
-                <!-- Icon Divider-->
-                <div class="divider-custom">
-                    <div class="divider-custom-line"></div>
-                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                    <div class="divider-custom-line"></div>
-                </div>
-                <!-- Contact Section Form-->
                 <div class="row">
                     <div class="col-lg-8 mx-auto">
-                        <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19.-->
-                        <form id="contactForm" name="sentMessage" novalidate="novalidate">
-                            <div class="control-group">
-                                <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                                    <label>Email Address</label>
-                                    <input class="form-control" id="email" type="email" placeholder="Email Address" required="required" data-validation-required-message="Please enter your email address." />
-                                    <p class="help-block text-danger"></p>
-                                </div>
+                        <p class="lead">Covid Status:</p>
+                        <div class="control-group">
+                            <div class="form-group controls mb-0 pb-2">
+                                <input class="form-control" id="covid_status" type="text" placeholder="Covid Status" required="required" data-validation-required-message="Please enter your covid status." />
                             </div>
-                            <div class="control-group">
-                                <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                                    <label>Password</label>
-                                    <input class="form-control" id="phone" type="tel" placeholder="Password" required="required" data-validation-required-message="Please enter your phone number." />
-                                    <p class="help-block text-danger"></p>
-                                </div>
-                            </div>
-                            <br />
-                            <div id="success"></div>
-                            <div class="form-group"><button class="btn btn-primary btn-xl" id="sendMessageButton" type="submit">Submit</button></div>
-                        </form>
+                        </div>
+                        <p class="lead">Street:</p>
+                        <div class="control-group">
+                            <div class="form-group controls mb-0 pb-2">
+                                <input class="form-control" id="street" type="text" placeholder="Street Address" required="required" data-validation-required-message="Please enter your street address." />
+                            </div>  
+                        </div>
+                        <p class="lead">City:</p>
+                        <div class="control-group">
+                            <div class="form-group controls mb-0 pb-2">
+                                <input class="form-control" id="city" type="text" placeholder="City" required="required" data-validation-required-message="Please enter your city." />
+                            </div>  
+                        </div>
+                        <p class="lead">State:</p>
+                        <div class="control-group">
+                            <div class="form-group controls mb-0 pb-2">
+                                <input class="form-control" id="state" type="text" placeholder="State" required="required" data-validation-required-message="Please enter your state." />
+                            </div>  
+                        </div>
+                        <p class="lead">Zipcode:</p>
+                        <div class="control-group">
+                            <div class="form-group controls mb-0 pb-2">
+                                <input class="form-control" id="zipcode" type="text" placeholder="Zipcode" required="required" data-validation-required-message="Please enter your zipcode." />
+                            </div>  
+                        </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </header>
+        
         <!-- Copyright Section-->
         <div class="copyright py-4 text-center text-white">
             <div class="container"><small>Copyright © Your Website 2020</small></div>
